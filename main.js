b8681630757bcb9616e751351810a8f71f54acc1
windomake <windomake@gmail.com>
2023-06-23 18:44:41 +0300

    Number 10

--- main.js
+++ main.js
@@ -30,3 +30,43 @@ console.log(null == false);
 console.log(1 === true);
 console.log(true == 1);
 console.log('Лавка' === undefined);
+// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?');
+// if (year == 2015) {
+//   alert('Вы правы!');
+//   alert('Вы такой умный!');
+// }
+// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?');
+// if (year < 2015) {
+//   alert('Это слишком рано...');
+// } else if (year > 2015) {
+//   alert('Это поздновато');
+// } else {
+//   alert('Верно!');
+// }
+// let accessAllowed;
+// let age = prompt('Сколько вам лет?');
+// if (age > 18) {
+//   accessAllowed = true;
+// } else {
+//   accessAllowed = false;
+// }
+// alert(accessAllowed);
+// let accessAllowed = age > 18 ? true : false;
+let year = prompt('В каком году создали интернет?');
+if (year < 1969) {
+  alert('Это рано...');
+} else if (year > 1969) {
+  alert('Это поздно');
+} else {
+  alert('Верно!');
+}
+
+if (age < 3) {
+  message = 'Здравствуй, малыш!';
+} else if (age < 18) {
+  message = 'Привет!';
+} else if (age < 100) {
+  massage = 'Здравствуйте!';
+} else {
+  massage = 'Какой необычный возраст!';
+}
