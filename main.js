ac9e3b9a6d96b3a30ada59ad1518b5e7276cc780
buka21 <petrushina_ekaterinka@mail.ru>
2023-06-26 21:50:46 +0300

    add files

--- main.js
+++ main.js
@@ -1,72 +1,36 @@
-let a = 6;
-let b = 9;
-a++;
-console.log(a + b);
-console.log(a - b);
-console.log(b / a);
-console.log(a ** 2 * b);
-console.log(b % a);
-console.log(`${a} + ${b}`);
-console.log('a' + b);
-console.log(b + false);
-console.log(null + a);
-console.log(a + true + ' b');
-console.log(a + undefined);
-console.log(a - a);
-console.log(2 > 1);
-console.log(7 <= 9);
-console.log(5 == 1);
-console.log(15 != 7);
-console.log(7 > 7);
-console.log('я' < 'Я');
-console.log('торт' >= 'тон');
-console.log('075' == 12);
-console.log('075' == 75);
-console.log('05' === 5);
-console.log('тара' === 'тара');
-console.log(null == undefined);
-console.log(null == 0);
-console.log(null == false);
-console.log(1 === true);
-console.log(true == 1);
-console.log('Лавка' === undefined);
-// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?');
-// if (year == 2015) {
-//   alert('Вы правы!');
-//   alert('Вы такой умный!');
+// function calc(a, b, operation) {
+//   if (operation === 'sum') {
+//     console.log(a + b);
+//   } else if (operation === 'min') {
+//     console.log(a - b);
+//   } else if (operation === 'mul') {
+//     console.log(a * b);
+//   } else if (operation === 'del') {
+//     console.log(a / b);
+//   }
 // }
-// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?');
-// if (year < 2015) {
-//   alert('Это слишком рано...');
-// } else if (year > 2015) {
-//   alert('Это поздновато');
-// } else {
-//   alert('Верно!');
-// }
-// let accessAllowed;
-// let age = prompt('Сколько вам лет?');
-// if (age > 18) {
-//   accessAllowed = true;
-// } else {
-//   accessAllowed = false;
-// }
-// alert(accessAllowed);
-// let accessAllowed = age > 18 ? true : false;
-let year = prompt('В каком году создали интернет?');
-if (year < 1969) {
-  alert('Это рано...');
-} else if (year > 1969) {
-  alert('Это поздно');
-} else {
-  alert('Верно!');
-}
+// calc(5, 3, 'sum'); //8
+// calc(9, 3, 'min'); //6
+// calc(2, 5, 'mul'); //10
+// calc(4, 2, 'del'); //2
 
-if (age < 3) {
-  message = 'Здравствуй, малыш!';
-} else if (age < 18) {
-  message = 'Привет!';
-} else if (age < 100) {
-  massage = 'Здравствуйте!';
-} else {
-  massage = 'Какой необычный возраст!';
+function calc2(a, b, operation) {
+  switch (operation) {
+    case 'sum':
+      console.log(a + b);
+      break;
+    case 'min':
+      console.log(a - b);
+      break;
+    case 'mul':
+      console.log(a * b);
+      break;
+    case 'del':
+      console.log(a / b);
+      break;
+  }
 }
+calc2(8, 1, 'sum');
+calc2(7, 5, 'min');
+calc2(3, 3, 'mul');
+calc2(6, 3, 'del');
